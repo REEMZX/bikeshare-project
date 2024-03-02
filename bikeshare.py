4b222bdf6cbb2ec64155640a91067b56e54a692a
Reem <alzahrani.s.reem@gmail.com>
2024-03-02 21:46:25 +0300

    Update bikeshare.py deleted comment

--- bikeshare.py
+++ bikeshare.py
@@ -222,8 +222,7 @@ def main():
             break
 
         #restart = input('\nWould you like to restart? Enter \'yes\' or \'no\'.\n')
-        #if restart.lower() != 'yes':
-            #break
+       
 
 if __name__ == "__main__":
     main()
